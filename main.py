c97ef9b21253fc4c1aa6a4bd21b5485be7da70ac
adem275045 <ettaki23@gmail.com>
2023-03-30 13:28:37 +0530

    Added titles to columns

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas
+
 
 st.set_page_config(layout="wide")
 
@@ -14,3 +16,17 @@ with col2:
 
 contact_me = '''Below you can find the apps I built while learning Python. Feel free to contact me!'''
 st.write(contact_me)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+#displays 0-10 rows in column3
+with col3:
+    for index, row in df[:10].iterrows():
+        st.title(row["title"])
+
+#displays 10-20 rows in column4
+with col4:
+    for index, row in df[10:].iterrows():
+        st.title(row["title"])
