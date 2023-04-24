0fbfff4c51e68381d6f3735f5ae3f003f179b0bd
adem275045 <ettaki23@gmail.com>
2023-04-24 12:34:11 +0530

    Send emails using forms

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,8 +1,20 @@
 import streamlit as st
+from send_email import send_email
+
 
 st.header("Contact Me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your Email Address")
     user_message = st.text_area("Your message")
+    # along with message user_email ID is also added in the mail.
+    message = f"""\
+Subject: New Email from {user_email}
+
+From: {user_email}
+{user_message}
+"""
     button = st.form_submit_button("Submit")
+    if button:
+        send_email(message)
+        st.info("Email was sent successfully.")
--- send_email.py
+++ send_email.py
@@ -1,20 +1,16 @@
 import smtplib,ssl
 
-host = "smtp.gmail.com"
-port = 465
-context = ssl.create_default_context()
 
-username = "ettaki23@gmail.com"
-password = "ngbkrrgqsjdkwjac"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    context = ssl.create_default_context()
+    username = "ettaki23@gmail.com"
+    password = "ngbkrrgqsjdkwjac"
+    receiver = "adarshmanjunath88@gmail.com"
 
-receiver = "adarshmanjunath88@gmail.com"
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.ehlo()
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-message = '''
-Hi,
-How are you?
-'''
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.ehlo()
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
